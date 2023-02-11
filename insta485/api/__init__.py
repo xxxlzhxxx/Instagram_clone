f9240121f4e0449a07bc69dc0d3c9710938c508f
xxxlzhxxx <zihengsjtu@gmail.com>
2023-02-11 16:55:58 -0500

    first 3 api basically finished, didn't run test

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -1,6 +1,4 @@
 """Insta485 REST API."""
-
-
 from insta485.api.posts import get_post
 import insta485.api  # noqa: E402  pylint: disable=wrong-import-position
 import insta485.views  # noqa: E402  pylint: disable=wrong-import-position
--- tests/app_tests/utils.py
+++
@@ -1,6 +0,0 @@
-"""P2 test suite utility functions."""
-import pathlib
-
-
-# Directory containing unit tests
-TEST_DIR = pathlib.Path(__file__).parent
