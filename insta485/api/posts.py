ffd8e35fc2ee01d35258a298d11c16a4cc0d1bfe
xxxlzhxxx <zihengsjtu@gmail.com>
2023-02-12 21:03:45 -0500

    posts line 34

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -31,7 +31,8 @@ def get_post_list():
 
     size = flask.request.args.get("size", default=10, type=int)
     page = flask.request.args.get("post", default=0, type=int)
-    largest_post_id = connection.execute("SELECT MAX(postid) FROM posts").fetchone()
+    largest_post_id = connection.execute("SELECT MAX(postid) as m FROM posts").fetchone()['m']
+
     postid_lte = flask.request.args.get("postid_lte", default=largest_post_id, type=int)
     cur = connection.cursor()
     cur = connection.execute(
